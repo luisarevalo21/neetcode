3f2463cbcc457da772eab0e948a405fa6460f14e
luisarevalo21 <arevaloluis89@yahoo.com>
2025-01-20 22:17:55 -0800

    completed optimized solution for group-anagram; ;

--- arrays/group-anagrams.js
+++ arrays/group-anagrams.js
@@ -1,7 +1,9 @@
 /**
  * @param {string[]} strs
  * @return {string[][]}
+ *
  */
+//burte force!
 var groupAnagrams = function (strs) {
   const map = new Map();
 
@@ -18,4 +20,41 @@ var groupAnagrams = function (strs) {
   }
 
   return [...map.values()];
+};
+
+//optimzied
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  //create the index array from 0 -26 subtract charactat z- a to place it in the array
+
+  const array = [];
+  for (let i = 0; i < strs.length; i++) {
+    const newArray = new Array(26).fill(0);
+
+    for (let j = 0; j < strs[i].length; j++) {
+      const index = strs[i].charCodeAt(j) - "a".charCodeAt(0);
+      newArray[index] = newArray[index] + 1;
+    }
+
+    array.push(newArray);
+  }
+
+  const map = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    if (map[array[i]] !== undefined) {
+      const res = map[array[i]];
+
+      res.push(strs[i]);
+
+      map[array[i]] = res;
+    } else {
+      map[array[i]] = [strs[i]];
+    }
+  }
+
+  return Object.values(map);
 };
